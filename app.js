de0232c9728c9d9b0e6f9a848112c03816201142
بييبلاتنمك <83167483+prolisey@users.noreply.github.com>
2023-03-12 11:17:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function solution() {
+let a = document.getElementById('a').value;
+let b = document.getElementById('b').value;
+document.getElementById('sum').innerText = +a + +b;
+}
+
+function dfg(){
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    document.getElementById('sum').innerText = +a - +b;
+}
+
+function sdf () {
+let a = document.getElementById('a').value;
+let b = document.getElementById('b').value;
+document. getElementById("sum").innerText = +a * +b;
+}
+
+function asd () {
+let a = document.getElementById('a'). value;
+let b = document.getElementById ('b'). value;
+document. getElementById("sum").innerText = +a / +b;
+}
